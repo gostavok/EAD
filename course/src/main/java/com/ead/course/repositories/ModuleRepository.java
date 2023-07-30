0dec373c68d0cda34fd25855ddf24a2bfa2db5c7
gustavo <g11henrique@hotmail.com>
2023-07-29 22:45:08 -0300

    Add LAZY, Query, EntityGraph

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,11 +1,22 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    @EntityGraph(attributePaths = {"course"})
+    ModuleModel findByTitle(String title);
+
+    @Query(value = "SELECT * FROM TB_MODULES WHERE course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
+
 }
