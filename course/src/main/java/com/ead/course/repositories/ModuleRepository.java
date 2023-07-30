cd603a242a5278c134ad0ac563499afb1e76a7d4
gustavo <g11henrique@hotmail.com>
2023-07-30 13:26:28 -0300

    Add ModuleController

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -19,4 +20,6 @@ public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
     @Query(value = "SELECT * FROM TB_MODULES WHERE course_course_id = :courseId", nativeQuery = true)
     List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
 
+    @Query(value = "SELECT * FROM TB_MODULES WHERE course_course_id = :courseId AND module_id = :moduleId", nativeQuery = true)
+    Optional<ModuleModel> findAllModulesIntoCourse(@Param("moduleId") UUID moduleId, @Param("courseId") UUID courseId);
 }
