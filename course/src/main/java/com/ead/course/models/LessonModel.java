9f21333c546fcfe68f497dafce96efabaee39a45
gustavo <g11henrique@hotmail.com>
2023-07-29 23:19:11 -0300

    Add delete course, module, lesson

--- course/src/main/java/com/ead/course/models/LessonModel.java
+++ course/src/main/java/com/ead/course/models/LessonModel.java
@@ -19,7 +19,7 @@ public class LessonModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID moduleId;
+    private UUID lessonId;
 
     @Column(nullable = false, length = 150)
     private String title;
