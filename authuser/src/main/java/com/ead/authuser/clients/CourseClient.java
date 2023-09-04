91c8cda8d6b3a5d2beb79008d1284301ba43b445
gustavo <g11henrique@hotmail.com>
2023-09-03 23:01:12 -0300

    Add retry

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,6 +34,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDto> searchResult = new ArrayList<>();
         String url = REQUEST_URL_COURSE + utilsService.createUrl(userId, pageable);
@@ -49,5 +51,10 @@ public class CourseClient {
         }
         log.info("Ending request /courses userId {}", userId);
         return new PageImpl<>(searchResult);
+    }
+
+    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Inside retry retryfallback , cause - {}", t.toString());
+        return new PageImpl<>(new ArrayList<>());
     }
 }
