e045268c25a133705364a715713709d6e7ebd9ec
gustavo <g11henrique@hotmail.com>
2023-11-05 12:10:44 -0300

    Add security

--- authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,18 +1,17 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDto;
-import com.ead.authuser.dtos.UserDto;
-import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
-import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,6 +28,10 @@ public class InstructorController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> sabeSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto){
 
@@ -37,9 +40,12 @@ public class InstructorController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         }
 
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR).orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+
         UserModel userModel = userModelOptional.get();
         userModel.setUserType(UserType.INSTRUCTOR);
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.getRoles().add(roleModel);
         userService.updateUser(userModel);
         log.info("User saved successfully userId {}", userModel.getUserId());
         return ResponseEntity.status(HttpStatus.OK).body(userModel);
--- authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -2,6 +2,7 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Log4j2
@@ -29,10 +29,16 @@ public class UserCourseController {
     @Autowired
     UserService userService;
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/users/{userId}/courses")
-    public ResponseEntity<Page<CourseDto>> getAllCoursesByUser(@PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
-                                                               @PathVariable(value = "userId") UUID userId){
-        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
+    public ResponseEntity<Object> getAllCoursesByUser(@PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
+                                                      @PathVariable(value = "userId") UUID userId,
+                                                      @RequestHeader("Authorization") String token) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable, token));
     }
 
 }
